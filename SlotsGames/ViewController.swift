c950ff71f2374e5cccb029c86cf2927b677ab8d1
VeronicaRudiuk <veronicarudiuk@gmail.com>
2022-12-24 11:36:35 +0400

    Initial Commit

---
+++ SlotsGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlotsGames
+//
+//  Created by Veronica Rudiuk on 24.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
